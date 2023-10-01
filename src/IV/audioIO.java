5907c44da2931bc03767b425495d243cc63c0854
Bobtown1234321 <rymassel@gmail.com>
2023-10-01 17:47:20 -0500

    IDIDITYUS

--- src/IV/audioIO.java
+++ src/IV/audioIO.java
@@ -1,7 +1,6 @@
 package IV;
 
 import javax.sound.sampled.*;
-import java.io.ByteArrayOutputStream;
 
 
 public class audioIO {
@@ -10,12 +9,10 @@ public class audioIO {
     private static DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
     private static SourceDataLine source;
     private static TargetDataLine target;
-
-    private static final ByteArrayOutputStream audioOut = new ByteArrayOutputStream();
     private static final int BUFFERSIZE = 5520;
     private static int readBytes;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         try {
             source = (SourceDataLine) AudioSystem.getLine(info);
             info = new DataLine.Info(TargetDataLine.class, format);
@@ -28,19 +25,6 @@ public class audioIO {
         }
 
         byte[] data = new byte[BUFFERSIZE / 4];
-        Thread sourceThread = new Thread(() -> {
-            source.start();
-            while (true) {
-                source.write(data, 0, readBytes);
-            }
-        });
-
-        Thread targetThread = new Thread(() -> {
-            target.start();
-            while (true) {
-                readBytes = target.read(data, 0, data.length);
-            }
-        }); //End of targetThread
 
         Thread single = new Thread(() -> {
             target.start();
@@ -50,28 +34,8 @@ public class audioIO {
                 source.write(data, 0, readBytes);
             }
         });
-
-        Thread testThread = new Thread(() -> {
-            int sentinal = 0;
-            while (sentinal < 200) {
-                System.out.println("Target: ");
-                System.out.println("Running: " + target.isRunning());
-                System.out.println("Active:  " + target.isActive());
-                System.out.println("BytesRead: " + target.available() + " Source: " + source.available());
-                System.out.println("Data.length: " + data.length);
-                System.out.println("Sentinal " + sentinal);
-                sentinal++;
-            }
-        });
-
         //running
-
-        //targetThread.start();
-        //sourceThread.start();
         single.start();
-
-        testThread.start();
-
 
     }
 }
